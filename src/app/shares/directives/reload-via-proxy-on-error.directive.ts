588f21fc72ce718338fe01af94845856f5f6049b
Le Cong Hau <anhlahau.hl@gmail.com>
2022-01-09 00:36:16 +0900

    add loading gif on image error

--- src/app/shares/directives/reload-via-proxy-on-error.directive.ts
+++ src/app/shares/directives/reload-via-proxy-on-error.directive.ts
@@ -1,5 +1,6 @@
 import {Directive, ElementRef, Input} from '@angular/core';
 import {httpGetAsync} from "../utils/http-utils";
+import {spinnerBase64} from "../constants/base64";
 
 @Directive({
   selector: '[reloadViaProxyOnError]'
@@ -24,16 +25,29 @@ export class ReloadViaProxyOnErrorDirective {
   }
 
   tryLoadImageViaProxy(event: any) {
-    if (!event.target.src.startsWith('data')) {
+    const originalSrc = event.target.src;
+    const originalStyle = {...event.target.style};
+    event.target.src = spinnerBase64;
+    event.target.style.width = "unset";
+    event.target.style.height = "unset";
+    event.target.style.margin = "auto";
+    event.target.style.top = "0";
+    event.target.style.right = "0";
+    if (!originalSrc.startsWith('data')) {
       console.log("trying to reload image via proxy");
       httpGetAsync(
-        event.target.src,
+        originalSrc,
         true,
         true,
         {contentType: 'base64'},
         this.headers
       ).subscribe((data) => {
         event.target.src = "data:image/png;base64, " + data;
+        event.target.style.width = originalStyle.width;
+        event.target.style.height = originalStyle.height;
+        event.target.style.margin = originalStyle.margin;
+        event.target.style.top = originalStyle.top;
+        event.target.style.right = originalStyle.right;
       })
     }
   }
