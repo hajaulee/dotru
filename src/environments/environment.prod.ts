c0c0662899f644302935a4b5be9fadbbd42a5f63
Le Cong Hau <anhlahau.hl@gmail.com>
2022-01-11 23:36:04 +0900

    add build date to version number

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: 'v0.0.34'
+  version: 'v0.0.35.22011114.1212121222'
 };
--- update_version.js
+++ update_version.js
@@ -1,20 +1,21 @@
 const replace = require('replace-in-file');
 
-var newVersion = "";
+let newVersion = "";
 
 function increaseVersionNumber(version) {
   console.log("Current version:", version);
-  let [major, minor, patch] = version.slice(1).split(".")
+  let [major, minor, patch, _] = version.slice(1).split(".")
+  let buildDate  = new Date().toISOString().replace(/[-T:.]/g, '').slice(2, 10)
   console.log({major, minor, patch});
   patch = parseInt(patch) + 1;
   console.log("Increase patch to:", patch);
 
-  newVersion = `v${major}.${minor}.${patch}`;
+  newVersion = `v${major}.${minor}.${patch}.${buildDate}`;
   return newVersion;
 }
 const options = {
     files: 'src/environments/environment.prod.ts',
-    from: /v\d.\d.\d*/g,
+    from: /v\d.\d.\d.\d*/g,
     to: (match) => increaseVersionNumber(match),
     allowEmptyPaths: false,
 };
